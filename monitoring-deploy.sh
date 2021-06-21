1792ca2cb371fa24d3b0ae04e986a0ce03ea7e2c
Ubuntu <ubuntu@ip-172-31-68-135.ec2.internal>
2021-06-21 21:01:04 +0000

    Add more steps on deploy script

--- monitoring-deploy.sh
+++ monitoring-deploy.sh
@@ -1,6 +1,18 @@
+# Deploy monitoring environment on Kubernetes
+
+# Creating Namespace and Secret
 kubectl apply -f grafana-monitoring.yaml
+
+# Install Prometheus and Grafana via Helm
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm install prometheus bitnami/kube-prometheus -n monitoring
 helm install grafana -n monitoring -f grafana-values.yaml bitnami/grafana
+
+# Creating Configmap
 kubectl apply -f grafana-cm.yaml
+
+# Configuring Prometheus datasource on Grafana
+export LB=$(kubectl -n monitoring get svc grafana -o json |jq .status.loadBalancer.ingress[0].hostname | sed 's/"//g')
+
+curl "http://admin:root@${LB}/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"Prometheus","type":"prometheus","url":"http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090","access":"proxy","isDefault":true}'
