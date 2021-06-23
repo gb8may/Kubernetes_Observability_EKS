b83422542f45368215a970a79e847069d5ec8d48
Ubuntu <ubuntu@ip-172-31-68-135.ec2.internal>
2021-06-23 17:10:09 +0000

    Add new files

--- monitoring-deploy.sh
+++ monitoring-deploy.sh
@@ -13,6 +13,8 @@ helm install grafana -n monitoring -f grafana-values.yaml bitnami/grafana
 kubectl apply -f grafana-cm.yaml
 
 # Configuring Prometheus datasource on Grafana
+sleep 180 #Wait for Granafa become available
+
 export LB=$(kubectl -n monitoring get svc grafana -o json |jq .status.loadBalancer.ingress[0].hostname | sed 's/"//g')
 
 curl "http://admin:root@${LB}/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"Prometheus","type":"prometheus","url":"http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090","access":"proxy","isDefault":true}'
