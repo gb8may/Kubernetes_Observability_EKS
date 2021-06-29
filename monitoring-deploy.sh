5590f83ec9217786ea37dce4751db101630a57db
Ubuntu <ubuntu@ip-172-31-68-135.ec2.internal>
2021-06-29 23:17:25 +0000

    Add fixes to script

--- monitoring-deploy.sh
+++ monitoring-deploy.sh
@@ -1,32 +1,64 @@
 # Deploy monitoring environment on Kubernetes
 
 # Creating Namespace and Secret
+echo "Configuring namespace and other components.."
 kubectl apply -f grafana-monitoring.yaml
 
 # Deploy kube-state-metrics
 kubectl apply -f ./kube-state-metrics/.
 
 # Install Prometheus and Grafana via Helm
+echo "Installing Prometheus.."
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm install prometheus -n monitoring -f prometheus-values.yaml bitnami/kube-prometheus
+cont=0
+max=10
+while [ "`kubectl -n monitoring get pods |grep prometheus-prometheus-kube-prometheus-prometheus-0 |grep '2/2' |grep Running 2>/dev/null`" = "" -a $cont -le $max ] ; do
+    sleep 5s
+    cont=`echo "$cont+1"|bc`
+    echo -n '.'
+done
+if [ "`kubectl -n monitoring get pods |grep prometheus-prometheus-kube-prometheus-prometheus-0 |grep '2/2' |grep Running 2>/dev/null`" = "" ] ; then
+    echo 'Something wrong with Prometheus! Exiting...'
+    exit 1
+fi
+
+echo "Installing Grafana"
 helm install grafana -n monitoring -f grafana-values.yaml bitnami/grafana
+cont=0
+max=10
+while [ "`kubectl -n monitoring get pods |grep grafana |grep '1/1' |grep Running 2>/dev/null`" = "" -a $cont -le $max ] ; do
+    sleep 5s
+    cont=`echo "$cont+1"|bc`
+    echo -n '.'
+done
+if [ "`kubectl -n monitoring get pods |grep grafana |grep '1/1' |grep Running 2>/dev/null`" = "" ] ; then
+    echo 'Something wrong with Grafana! Exiting...'
+    exit 1
+fi
 
 # Creating Configmap
 kubectl apply -f grafana-cm.yaml
 
 # Configuring Prometheus datasource on Grafana
 echo "Configuring Prometheus Datasource and Grafana Dashboard...."
-sleep 180 #Wait for Granafa become available
 
+PW=$(echo "cm9vdA==" | base64 --decode)
 export LB=$(kubectl -n monitoring get svc grafana -o json |jq .status.loadBalancer.ingress[0].hostname | sed 's/"//g')
 
-curl "http://admin:root@${LB}/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"Prometheus","type":"prometheus","url":"http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090","access":"proxy","isDefault":true}'
+cont=0
+max=10
+while [ "`curl -o - http://${LB} 2>/dev/null`" = "" -a $cont -le $max ] ; do
+    sleep 5s
+    cont=`echo "$cont+1"|bc`
+    echo -n '.'
+done
+
+curl "http://admin:${PW}@${LB}/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"Prometheus","type":"prometheus","url":"http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090","access":"proxy","isDefault":true}'
 
 # Configuring Dashboard
-PW=$(echo "cm9vdA==" | base64 --decode)
-
-curl -i -u admin:${PW} -H "Content-Type:application/json;charset=UTF-8" -X POST http://${LB}/api/dashboards/db -d @dashboard.json 
+curl -i -u admin:${PW} -H "Content-Type:application/json;charset=UTF-8" -X POST http://${LB}/api/dashboards/db -d @dashboard.json
 
 echo ""
-echo "Your dashboard is UP at http://${LB}/d/7w4JC6R7z/load-test-test-01"
+echo "Your dashboard is UP at http://${LB}/d/7w4JC6R7z"
